072190065cb18342c056e8d62c9d0617906b8af5
suzushima-ryousuke <66098548+suzushima-ryousuke@users.noreply.github.com>
2020-07-10 17:27:51 +0900

    Initial project setup

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by アルティメイトプロジェクト on 2020/07/10.
+//  Copyright © 2020 アルティメイトプロジェクト. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
